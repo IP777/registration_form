721bb6315aa7b8d9afaab5d19cd97d3476fa008c
IP777 <ivanov_piter@ukr.net>
2020-06-23 20:17:33 +0300

    edits-1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCvNV3g_80avFyzr6MM6-fEbxLplPe6Bw0",
+	authDomain: "registration-form-firebase.firebaseapp.com",
+	databaseURL: "https://registration-form-firebase.firebaseio.com",
+	projectId: "registration-form-firebase",
+	storageBucket: "registration-form-firebase.appspot.com",
+	messagingSenderId: "504812036054",
+	appId: "1:504812036054:web:556724044759ce05e60e77",
+	measurementId: "G-E3YV2PT00V",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
 	<Provider store={store}>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import * as firebase from "firebase";
 
-const HomePage = () => <h3>Home Page</h3>;
+export default class HomePage extends Component {
+	state = { nick: "", name: "", age: null };
 
-export default HomePage;
+	componentDidMount() {
+		const rootRef = firebase.database().ref().child("react");
+		//const speedRef = rootRef.child("speed");
+
+		// speedRef.on("value", (snap) => {
+		// 	this.setState({ speed: snap.val(), user: snap.val() });
+		// });
+
+		rootRef.on("value", (snap) => {
+			console.log(snap.val().name);
+			this.setState({
+				nick: snap.val().nick,
+				name: snap.val().name,
+				age: snap.val().age,
+			});
+		});
+	}
+
+	render() {
+		return (
+			<ul>
+				<li>
+					<h3>Slide 1 //name: {this.state.name}</h3>
+				</li>
+				<li>
+					<h3>Slide 2 //name: {this.state.nick}</h3>
+				</li>
+				<li>
+					<h3>Slide 3 //age: {this.state.age}</h3>
+				</li>
+				<li>
+					<h3>Slide 4</h3>
+				</li>
+			</ul>
+		);
+	}
+}
